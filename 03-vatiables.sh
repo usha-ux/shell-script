fb234aea5006b6a25ea72925428bd645eab11303
UshaKondaka <jinagagiribabu@outlook.com>
2025-02-24 16:04:49 +0530

    changed conversation

---
+++ 03-vatiables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1::hi $PERSON2!"
+echo "$PERSON2::hello $PERSON1"
+echo "$PERSON1::how are you?"
+echo "$PERSON2::I am fine, what about you?"
